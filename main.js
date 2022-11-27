dcfcfaa8bdd282b524c447ea39bd903d4b36a854
Skylar Grant <official.xian@gmail.com>
2022-11-26 19:44:31 -0500

    a

--- main.js
+++ main.js
@@ -1,31 +1,32 @@
 //This example shows how to setup the pin for write mode with the default state as "on". Why do this? It can sometimes be useful to reverse the default initial state due to wiring or uncontrollable circumstances.
 //var gpio = require('rpi-gpio');
 
-//gpio.setup(7, gpio.DIR_OUT, write);
+gpio.setup(7, gpio.DIR_OUT, write);
 
-// function augerOn(err) {
-//     if (err) throw err;
-//     gpio.write(7, true, function(err) {
-//         if (err) throw err;
-//         console.log('Auger turned on.');
-//     });
-// }
-
-// function augerOff(err) {
-//     if (err) throw err;
-//     gpio.write(7, false, function(err) {
-//         if (err) throw err;
-//         console.log('Auger turned off.');
-//     });
-// }
-
-function augerOff() {
-    console.log('Auger turned off.');
+function augerOn(err) {
+    if (err) throw err;
+    gpio.write(7, true, function(err) {
+        if (err) throw err;
+        console.log('Auger turned on.');
+    });
 }
 
-function augerOn() {
-    console.log('Auger turned on.');
+function augerOff(err) {
+    if (err) throw err;
+    gpio.write(7, false, function(err) {
+        if (err) throw err;
+        console.log('Auger turned off.');
+    });
 }
+
+// Identical functions as above for debugging without gpio
+// function augerOff() {
+//     console.log('Auger turned off.');
+// }
+
+// function augerOn() {
+//     console.log('Auger turned on.');
+// }
   
 function sleep(ms) {
     return new Promise((resolve) => {
