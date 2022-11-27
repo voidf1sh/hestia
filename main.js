9924b8bfdc06a292cdcb27a42a358143349962b4
Skylar Grant <official.xian@gmail.com>
2022-11-26 20:31:54 -0500

    change offtime

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ function sleep(ms) {
 
 async function cycleAuger(err) {
     if (err) throw err;
-    const offTime = 1500; //ms
+    const offTime = 1200; //ms
     const onTime = 500;   //ms
     augerOn();
     await sleep(onTime);
